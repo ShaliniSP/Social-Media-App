61aed74a54c132aaa607d9cc55e7983cb1ba8ffa
jayasurya.seenuvasan <suryaseenu@gmail.com>
2019-02-14 18:57:16 +0530

    css formatting

--- src/components/AppContainer/app-container.js
+++ src/components/AppContainer/app-container.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './app-container.css';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
-import * as bs from 'bootstrap/dist/css/bootstrap.css';
-import * as bst from 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 import PostsContainer from './../PostContainer/post-container.js';
 
@@ -10,7 +9,7 @@ class AppContainer extends Component {
     render() {
         return (
             <div>
-                <Navbar className={bs["nav"]} expand="true" fixed="top">
+                <Navbar className="nav" expand="true" fixed="top">
                     <Navbar.Brand href="#home" ><h1>SlActs</h1></Navbar.Brand>
                     <Button variant="outline-light">Login</Button>
                 </Navbar>
