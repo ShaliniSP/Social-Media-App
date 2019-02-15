7d6746a6efe70192a677dbd28dcdddd4fcfbd602
jayasurya.seenuvasan <suryaseenu@gmail.com>
2019-02-15 18:48:43 +0530

    frontend developement

--- src/dataservice/posts-service.js
+++ src/dataservice/posts-service.js
@@ -8,28 +8,28 @@ export default {
     // gets posts of a category and return is callback
     getPostsByCategoryName(categoryName, callback) {
         window.fetch(RESTAPI + '/api/acts/v1/categories/' + categoryName + '/acts')
-        .then(response => response.json())
-        .then(json => {
-            // console.log('getPostsByCategoryNameAPI', json);
-            return callback(json);
-        });
+            .then(response => response.json())
+            .then(json => {
+                // console.log('getPostsByCategoryNameAPI', json);
+                return callback(json);
+            });
     },
 
     // username and password in an object and return is callback
     login(unameAndPassword, callback) {
-        window.fetch(RESTAPI + '/api/account/signin',{
-          method:'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(unameAndPassword),
+        window.fetch(RESTAPI + '/api/account/signin', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(unameAndPassword),
         })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            return callback(json);
-        });
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                return callback(json);
+            });
     },
 
     upload(data, callback) {
@@ -49,48 +49,81 @@ export default {
     },
 
     signup(unameAndPassword, callback) {
-        window.fetch(RESTAPI + '/api/v1/users',{
-          method:'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(unameAndPassword),
+        window.fetch(RESTAPI + '/api/v1/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(unameAndPassword),
         })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            return callback(json);
-        });
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                return callback(json);
+            });
     },
 
     getAllPosts(callback) {
         window.fetch(RESTAPI + '/api/v1/acts')
-        .then(resp => {
-            if (resp.status === 200) {
-                return resp.json();
-            } else {
-                return [];
-            }
-        })
-        .then(json => {
-            console.log('getAllPostsResponse', json);
-            return callback(json);
-        });
+            .then(resp => {
+                if (resp.status === 200) {
+                    return resp.json();
+                } else {
+                    return [];
+                }
+            })
+            .then(json => {
+                console.log('getAllPostsResponse', json);
+                return callback(json);
+            });
     },
 
     getAllCats(callback) {
         window.fetch(RESTAPI + '/api/v1/categories')
-        .then((resp => {
-            if (resp.status === 200) {
-                return resp.json();
-            } else if (resp.status === 204) {
-                return [];
-            } else {
-                return [];
-            }
-        }))
-        .then(json => callback(json));
+            .then((resp => {
+                if (resp.status === 200) {
+                    return resp.json();
+                } else if (resp.status === 204) {
+                    return [];
+                } else {
+                    return [];
+                }
+            }))
+            .then(json => callback(json));
+    },
+
+    deletePost(actId, callback) {
+        window.fetch(RESTAPI + '/api/v1/acts/' + actId, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                if (response.status === 400) {
+                    return callback(false);
+                }
+                else {
+                    return callback(true);
+                }
+            })
+            .then(json => {
+                console.log(json);
+                return callback(json);
+            });
+    },
+
+    upvoteAct(actId, callback) {
+        window.fetch(RESTAPI + '/api/v1/acts/upvote', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([ actId ]),
+        });   
     },
 
     uploadPost(post, callback) {
